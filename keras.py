4cfcc21e4e59e2877e69e9c3732b89ada3e6f045
J <james.hyojae.lee@gmail.com>
2020-12-05 18:55:39 -0800

    first commit

---
+++ keras.py
@@ -0,0 +1,11 @@
+
+from numpy import loadtxt
+from tensorflow.keras.models import Sequential
+from keras.models import Sequential
+from keras.layers import Dense
+
+# load the dataset
+dataset = loadtxt('pima-indians-diabetes.csv', delimiter=',')
+# split into input (X) and output (y) variables
+X = dataset[:,0:8]
+y = dataset[:,8]
